cf7a58fdc834e32c54d4e252fa2d393661d4ef27
Reinardus Navellio <reintirta@gmail.com>
2023-10-16 22:04:00 +0200

    initial commit + file structure

---
+++ back-end/src/main/java/com/reinnav/pawdoptbackend/PawdoptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reinnav.pawdoptbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawdoptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawdoptBackendApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/reinnav/pawdoptbackend/PawdoptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinnav.pawdoptbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawdoptBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
