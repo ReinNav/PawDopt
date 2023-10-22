44e2ebaa68a0841885bc4292700821709797c514
Reinardus Navellio <reintirta@gmail.com>
2023-10-22 23:35:28 +0200

    Added index.html and testscript.js for lab 2 exercise

---
+++ front-end/testscript.js
@@ -0,0 +1,22 @@
+document.getElementById("getAllDogsBtn").addEventListener("click", function() {
+    fetch("http://localhost:8080/api/dog")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("apiResponse").value = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        document.getElementById("apiResponse").value = "Error: " + error;
+    });
+});
+
+document.getElementById("getSpecificDogBtn").addEventListener("click", function() {
+    const dogId = document.getElementById("dogId").value;
+    fetch("http://localhost:8080/api/dog" + dogId)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("apiResponse").value = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        document.getElementById("apiResponse").value = "Error: " + error;
+    });
+});
