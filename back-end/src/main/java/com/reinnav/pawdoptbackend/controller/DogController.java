64c34a48a2c8366e6aa7da5930a348eb1298c606
Reinardus Navellio <reintirta@gmail.com>
2023-10-22 23:17:32 +0200

    Added save + delete in service, updated DogController

--- back-end/src/main/java/com/reinnav/pawdoptbackend/controller/DogController.java
+++ back-end/src/main/java/com/reinnav/pawdoptbackend/controller/DogController.java
@@ -1,14 +1,46 @@
 package com.reinnav.pawdoptbackend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.reinnav.pawdoptbackend.model.Dog;
+import com.reinnav.pawdoptbackend.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/dog")
 public class DogController {
 
-    @GetMapping("/sayhello")
-    public String sayHello() {
-        System.out.println("Hello");
-        return "Hello";
+    @Autowired
+    private DogService dogService;
+
+    /**
+     * Responds to a get API, finding all instances of dog
+     * @return all dog instances
+     */
+    @GetMapping
+    public List<Dog> findAll() {
+        return dogService.findAll();
     }
+
+    /**
+     * Responds to a get API, finding a dog by ID
+     * @param id
+     * @return a dog instance with id
+     */
+    @GetMapping("/{id}")
+    public Dog findById(@PathVariable String id) {
+        return dogService.findById(id);
+    }
+
+    /**
+     * saves a new dog instance to DB
+     * @param dog new dog
+     * @return new dog saved
+     */
+    @PostMapping
+    public Dog create(@RequestBody Dog dog) {
+        return dogService.save(dog);
+    }
+
 }
--- back-end/src/main/java/com/reinnav/pawdoptbackend/service/DogService.java
+++ back-end/src/main/java/com/reinnav/pawdoptbackend/service/DogService.java
@@ -30,6 +30,24 @@ public class DogService {
         return dogRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Dog with ID " + id + " not found"));
     }
 
+    /**
+     * Saves a new dog instance to DB
+     * @param dog new Dog
+     * @return Dog saved
+     */
+    public Dog save(Dog dog){
+        return dogRepository.save(dog);
+    }
+
+    /**
+     * Deletes a dog with a certain id from DB
+     * @param id id of dog instance
+     * @return A dog with id
+     */
+    public void deleteById(String id){
+        dogRepository.deleteById(id);
+    }
+
 
 
 }
