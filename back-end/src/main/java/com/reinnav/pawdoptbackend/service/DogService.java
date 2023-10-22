3e6d9bdddd55f7a7e4d6570043c8d8904215bc2b
Reinardus Navellio <reintirta@gmail.com>
2023-10-22 22:17:39 +0200

    Added findById in DogService and custom exception

--- back-end/src/main/java/com/reinnav/pawdoptbackend/service/DogService.java
+++ back-end/src/main/java/com/reinnav/pawdoptbackend/service/DogService.java
@@ -1,5 +1,6 @@
 package com.reinnav.pawdoptbackend.service;
 
+import com.reinnav.pawdoptbackend.exception.EntityNotFoundException;
 import com.reinnav.pawdoptbackend.model.Dog;
 import com.reinnav.pawdoptbackend.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,16 @@ public class DogService {
     public List<Dog> findAll() {
         return dogRepository.findAll();
     }
+
+    /**
+     * Returns a dog with a certain id
+     * @param id id of dog instance
+     * @return A dog with id
+     */
+    public Dog findById(String id) {
+        return dogRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Dog with ID " + id + " not found"));
+    }
+
+
 
 }
