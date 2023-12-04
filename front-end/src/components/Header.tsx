e467d09f279357eace7b6f4ba48bef001806326c
Reinardus Navellio <reintirta@gmail.com>
2023-12-04 10:38:02 +0100

    change header title

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC<HeaderProps> = ({ username }) => {
     <header>
       <div className="flex-row" id="main-header">
         <img className="site-logo" src="pawdopt-logo.png" />
-        <h1 className="site-heading">Welcome, {username}</h1>
+        <h1 className="site-heading">Welcome to PawDopt</h1>
         <button className="btn secondary-btn">Sign in</button>
       </div>
     </header>
